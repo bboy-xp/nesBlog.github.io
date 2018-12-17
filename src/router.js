f59aecad8e973c4c0b92bb73ad166b69afcfa935
unknown <1169470657@qq.com>
2018-12-17 15:32:25 +0800

    test

--- src/router.js
+++ src/router.js
@@ -4,7 +4,6 @@ import Home from "./views/Home.vue";
 import IndexPage from "./views/IndexPage.vue"
 
 Vue.use(Router);
-
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
