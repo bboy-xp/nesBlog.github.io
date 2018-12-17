a3b9304e226c1c9b6cb2ccd55f6a0de114a5847b
unknown <1169470657@qq.com>
2018-12-17 15:12:29 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <button class="btn">sdfsdfs</button>
+    <div class="container with-title is-center">
+      <p class="title">asfsafes</p>
+      <p>safsefseaf</p>
+    </div>
+    <div class="container with-title is-dark">
+      <p class="title">asfsafes</p>
+      <p>safsefseaf</p>
+    </div>
+    <div class="container with-title is-dark is-rounded">
+      <p class="title">asfsafes</p>
+      <p>safsefseaf</p>
+    </div>
+    <div class="container with-title">
+      <h2 class="title">xp</h2>
+      <div>
+        <label>
+          <input type="radio" class="radio" name="answer">
+          <span>Yes</span>
+        </label>
+        <label>
+          <input type="radio" class="radio" name="answer">
+          <span>No</span>
+        </label>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+  name: "home"
+  // components: {
+  //   HelloWorld
+  // }
 };
 </script>
---
+++ src/views/IndexPage.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="indexPage">
+    <div class="header">
+      <img class="logo" src="../assets/logo.png" alt="404">
+      <div class="titleText">XP's blog</div>
+    </div>
+    <div class="container is-dark with-title absoluteContainer">
+      <p class="title">tep</p>
+      <h2 style="color: white">Happy everyday</h2>
+    </div>
+    <div class="balloon container with-title">
+      <h1 class="title">Balloons</h1>
+      <div class="messages">
+        <div class="message -left">
+          <i class="icon github is-large"></i>
+          <div class="balloon from-left">
+            <p>Hello Visitor!</p>
+          </div>
+        </div>
+        <div class="message -right">
+          <div class="balloon from-right">
+            <p>Welcome to my NES style blog.</p>
+          </div>
+          <i class="octocat animate"></i>
+        </div>
+        <div class="message -left">
+          <i class="icon github is-large"></i>
+          <div class="balloon from-left">
+            <p>
+              If you like, please give me
+              <i class="icon star"></i>
+            </p>
+            <div>
+              <label>
+                <input type="radio" name="answer" class="radio" @click="goToStar">
+                <span>Yes</span>
+              </label>
+              <label>
+                <input type="radio" name="answer" class="radio">
+                <span>No</span>
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+      <i class="icon heart is-medium"></i>
+    </div>
+    <footer class="footText">
+      <p>
+        <a href="#" style="color: #212529">Jser Club</a>
+        <span> - </span>
+        <a href="https://github.com/bboy-xp" style="color: #212529">@newbility-xp</a>
+      </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "indexPage",
+  methods: {
+    goToStar() {
+      location.href = "#";
+    }
+  }
+};
+</script>
+<style scoped>
+.indexPage {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+}
+.header {
+  text-align: center;
+}
+.logo {
+  height: 250px;
+  margin-top: 50px;
+}
+.titleText {
+  font-size: 60px;
+}
+.container {
+  width: 1000px;
+  margin-top: 20px;
+}
+.messages {
+  display: flex;
+  flex-direction: column;
+}
+.message {
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 15px;
+}
+.-left {
+  justify-content: flex-start;
+}
+.-right {
+  justify-content: flex-end;
+}
+.balloon {
+  margin-right: 30px;
+  margin-left: 30px;
+}
+.heart {
+  margin-right: 8px;
+}
+.footText {
+  /* justify-content: flex-end; */
+  position: absolute;
+  bottom: 15px;
+  color: black;
+}
+.absoluteContainer {
+  position: absolute;
+  width: 300px;
+  height: 200px;
+  right: 100px;
+  top: 30px
+}
+</style>
+
